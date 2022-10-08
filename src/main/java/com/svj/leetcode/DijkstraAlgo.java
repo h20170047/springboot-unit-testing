1a45aa810e37b8dca36666ac5a77d032773164a0
h20170047 <46812751+h20170047@users.noreply.github.com>
2022-10-09 02:13:33 +0530

    Adding Single Source All Destn ShortestPath Algo

---
+++ src/main/java/com/svj/leetcode/DijkstraAlgo.java
@@ -0,0 +1,67 @@
+package com.svj.leetcode;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+
+
+class GraphEdge {
+    Integer src;
+    Integer dsn;
+    Integer wt;
+    GraphEdge(int s, int d, int w){
+        src= s; dsn= d; wt= w;
+    }
+}
+
+public class DijkstraAlgo {
+    public int calculateDist(List<Integer[]> edges, int v, int src, int dsn) {
+        int eCount= edges.size();
+        Map<Integer, List<GraphEdge>> adjList= new HashMap<>();
+        Map<Integer, Integer> minDist= new HashMap<>();
+        Map<Integer, Set<Integer>> path= new HashMap<>();
+        IntStream.range(0, v)
+                        .forEach(k-> {
+                            minDist.put(k, Integer.MAX_VALUE);
+                            adjList.put(k, new LinkedList<>());
+                            path.put(k, new HashSet<>());
+                        });
+        minDist.put(src, 0);
+        int from, to, wt;
+        for(Integer[] arr: edges){
+            from= arr[0];
+            to= arr[1];
+            wt= arr[2];
+            GraphEdge edge= new GraphEdge(from, to, wt);
+            List<GraphEdge> neighbours= adjList.get(from);
+            neighbours.add(edge);
+        }
+        boolean[] visited= new boolean[v];
+        Queue<GraphEdge> queue= new PriorityQueue<>((GraphEdge e1, GraphEdge e2)-> e1.wt.compareTo(e2.wt));
+        queue.add(new GraphEdge(src, src, 0));
+        while (!queue.isEmpty()){
+            GraphEdge visitingNode= queue.poll();
+            if(visited[visitingNode.dsn]== false){
+                Set<Integer> pathSoFar= new HashSet<>();
+                pathSoFar.addAll(path.get(visitingNode.src));
+                pathSoFar.add(visitingNode.dsn);
+                path.put(visitingNode.dsn, pathSoFar);
+                for(GraphEdge edge: adjList.get(visitingNode.dsn)){
+                    from= edge.src; to= edge.dsn; wt= edge.wt;
+                    if(visited[to]== false && minDist.get(from)+wt< minDist.get(to)){
+                        // update weights, push to Queue
+                        minDist.put(to, minDist.get(from)+wt);
+                        queue.add(new GraphEdge(visitingNode.dsn, to, minDist.get(to)));
+                    }
+                }
+                visited[visitingNode.dsn]= true;
+            }
+        }
+        IntStream.range(0, v)
+                .forEach(k-> System.out.println(
+                        "Path from ".
+                                concat(String.valueOf(src)).concat(" to ").concat(String.valueOf(k))
+                                .concat(": ").concat(String.valueOf(path.get(k)))));
+        return minDist.get(dsn);
+    }
+}
---
+++ src/test/java/com/svj/leetcode/DijkstraAlgoTest.java
@@ -0,0 +1,66 @@
+package com.svj.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DijkstraAlgoTest {
+
+    @Test
+    public void g4gTest(){
+        DijkstraAlgo algo= new DijkstraAlgo();
+        List<Integer[]> edges= Arrays.asList(
+                new Integer[]{0, 1, 4},
+                new Integer[]{0, 7, 8},
+                new Integer[]{1, 2, 8},
+                new Integer[]{1, 7, 11},
+                new Integer[]{2, 3, 7},
+                new Integer[]{2, 8, 2},
+                new Integer[]{2, 5, 4},
+                new Integer[]{3, 4, 9},
+                new Integer[]{3, 5, 14},
+                new Integer[]{4, 5, 10},
+                new Integer[]{5, 6, 2},
+                new Integer[]{6, 7, 1},
+                new Integer[]{6, 8, 6},
+                new Integer[]{7, 8, 7}
+        );
+        assertThat(algo.calculateDist(edges, 9, 0, 8)).isEqualTo(14);
+    }
+
+    @Test
+    public void ustTest(){
+        DijkstraAlgo algo= new DijkstraAlgo();
+        List<Integer[]> edges= Arrays.asList(
+                new Integer[]{0, 1, 2},
+                new Integer[]{0, 2, 7},
+                new Integer[]{1, 2, 2},
+                new Integer[]{2, 1, 3},
+                new Integer[]{2, 3, 1},
+                new Integer[]{1, 4, 5},
+                new Integer[]{1, 3, 8},
+                new Integer[]{4, 3, 5},
+                new Integer[]{3, 4, 4}
+        );
+        assertThat(algo.calculateDist(edges, 5, 0, 4)).isEqualTo(7);
+    }
+
+    @Test
+    public void mitTest(){
+        DijkstraAlgo algo= new DijkstraAlgo();
+        List<Integer[]> edges= Arrays.asList(
+                new Integer[]{0, 1, 10},
+                new Integer[]{0, 2, 3},
+                new Integer[]{1, 2, 1},
+                new Integer[]{2, 1, 4},
+                new Integer[]{1, 3, 2},
+                new Integer[]{2, 4, 2},
+                new Integer[]{3, 4, 7},
+                new Integer[]{4, 3, 9}
+        );
+        assertThat(algo.calculateDist(edges, 5, 0, 4)).isEqualTo(5);
+    }
+}
